f96d10e0ac7f163de884b12d111895395b252f9c
mrizkyrn <mrizkyr2411@gmail.com>
2023-12-15 13:51:10 +0700

    add songs service

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,4 +9,26 @@ const mapAlbumsDBToModel = ({ id, name, year, created_at, updated_at }) => ({
    updatedAt: updated_at,
 });
 
-module.exports = { mapAlbumsDBToModel };
+const mapSongsDBToModel = ({
+   id,
+   title,
+   year,
+   genre,
+   performer,
+   duration,
+   albumId,
+   created_at,
+   updated_at,
+}) => ({
+   id,
+   title,
+   year,
+   genre,
+   performer,
+   duration,
+   albumId,
+   createdAt: created_at,
+   updatedAt: updated_at,
+});
+
+module.exports = { mapAlbumsDBToModel, mapSongsDBToModel };
