3d4a308df7c178a33104f2295ad269dd4f8be0bc
mrizkyrn <mrizkyr2411@gmail.com>
2023-12-15 13:14:16 +0700

    add ablums handlers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const dotenv = require('dotenv');
 const hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const ClientError = require('./exceptions/ClientError');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 dotenv.config();
 
 const init = async () => {
+   const albumsService = new AlbumsService();
+
    const server = hapi.server({
       port: process.env.PORT || 3000,
       host: process.env.HOST || 'localhost',
@@ -12,6 +18,46 @@ const init = async () => {
             origin: ['*'],
          },
       },
+   });
+
+   await server.register({
+      plugin: albums,
+      options: {
+         service: albumsService,
+         validator: AlbumsValidator,
+      },
+   });
+
+   server.ext('onPreResponse', (request, h) => {
+      const { response } = request;
+
+      // check if the response is an error
+      if (response instanceof Error) {
+         // client error handling
+         if (response instanceof ClientError) {
+            const newResponse = h.response({
+               status: 'fail',
+               message: response.message,
+            });
+            newResponse.code(response.statusCode);
+            return newResponse;
+         }
+
+         if (!response.isServer) {
+            return h.continue;
+         }
+
+         // server error handling
+         const newResponse = h.response({
+            status: 'error',
+            message: 'Maaf, terjadi kegagalan pada server kami.',
+         });
+         newResponse.code(500);
+         console.error(response);
+         return newResponse;
+      }
+
+      return h.continue;
    });
 
    await server.start();
